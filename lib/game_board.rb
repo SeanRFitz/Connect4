db1aa7796545ace7290b785f4e1ad9220bc7932c
Sean Fitzgerald <Sean.R.Fitzgerald@outlook.com>
2017-07-06 20:32:04 -0500

    Created and passed tests for the win?, drop, and tie? methods.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -13,5 +13,75 @@ class Board
 				])
 	end
 
+	def display
+		7.times do |row|
+			row -= 1
+			puts "|#{@rows[row].join("|")}|"
+		end
+		puts "|=============|"
+	end
 
+	def win?
+		# Check rows
+		7.times do |row|
+			row -= 1
+			cur_row = @rows[row]
+				if cur_row.count("X") < 4 && cur_row.count("O") < 4
+					next
+				else
+					counter, value = 0, "" 
+					cur_row.each do |cell|
+						if cell == "_"
+							value = ""
+							next
+						else
+							if value == ""
+								value = cell
+								counter += 1
+							elsif cell == value
+								counter += 1
+							else
+								value = cell
+								counter = 1
+							end
+
+							if counter == 4
+								return true
+							end
+						end
+					end
+				end
+		end
+		false
+	end
+
+	def tie?
+		if @rows.each do |row|
+			row.each do |cell|
+				if cell == "_"
+					return false
+				end
+			end
+		end
+		end
+		true
+	end
+
+	def drop(turn,column)
+		if turn % 2 == 0
+			disc = "O"
+		else
+			disc = "X"
+		end
+
+		column -= 1
+		cur_row = 6
+		while  @rows[cur_row][column] != "_"
+			cur_row -= 1
+			if cur_row < 0
+				return false
+			end
+		end
+		@rows[cur_row][column] = disc
+	end
 end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -2,6 +2,39 @@ require "game_board"
 
 describe Board do
 	subject(:board) {Board.new}
+	let(:winning_horizontalX) {
+		[
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","O","_","O"],
+			["O","X","X","X","X","O","O"],
+			["X","X","X","O","X","O","O"],
+		]
+	}
+	let(:winning_horizontalO) {
+		[
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["_","_","_","_","_","_","_"],
+			["X","X","X","O","O","O","O"],
+		]
+	}
+	let(:tie_board) {
+		[
+			["O","X","X","O","X","O","O"],
+			["O","X","O","O","O","X","X"],
+			["X","O","X","O","X","O","X"],
+			["X","O","X","X","O","O","X"],
+			["O","X","O","X","O","X","O"],
+			["O","X","X","O","X","O","O"],
+			["X","X","X","O","X","O","X"],
+		]
+	}
 
 	describe ".new" do
 		it "creates an empty board" do
@@ -17,16 +50,75 @@ describe Board do
 		end
 	end
 
-	describe ".win?" do
-		context "when a column of 4 Xs" do
+	describe "#win?" do
+		
+		context "when a column of 4 Os" do
+			before { board.rows = winning_horizontalO }
+
 			it "returns true" do
-				expect(Board.win?).to eql(true)
+				expect(board.win?).to eql(true)
 			end
 		end
 
-		context "when a column of 4 Os" do
+		context "when a column of 4 Xs" do
+			before { board.rows = winning_horizontalX }
+
 			it "returns true" do
-				expect(Board.win?).to eql(true)
+				
+				expect(board.win?).to eql(true)
+			end
+		end
+
+		context "when no one has 4 in a row" do
+			before { board.rows = tie_board }
+			it "returns false" do
+				expect(board.win?).to eql(false)
+			end
+		end
+	end
+
+	describe "#tie?" do
+		context "when the board is full" do
+			before { board.rows = tie_board }
+
+			it "returns true" do
+				expect(board.tie?).to eql(true)
+			end
+		end
+
+		context "when the board is not full" do
+			it "returns false" do
+				expect(board.tie?).to eql(false)
+			end
+		end
+	end
+
+	describe "#drop" do
+		context "on the first turn the player drops disc in the first column" do
+			before do
+				turn = 1
+				column = 1
+				board.drop(turn,column) 
+			end
+
+			it "adds an X" do
+				expect(board.rows[6][0]).to eql("X")
+			end
+		end
+
+		context "on the 2nd turn the player drops a disc on top of player 1's" do
+			before do 
+				turn = 1
+				column = 1
+				board.drop(turn,column)
+				turn = 2
+				board.drop(turn,column)
+			end
+
+			it "adds an O on top of X" do
+
+				expect(board.rows[6][0]).to eql("X")
+				expect(board.rows[5][0]).to eql("O")
 			end
 		end
 	end
